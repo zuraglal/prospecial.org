d2e3bb97e30813f77ded77b812bdbe98b5a0a272
zuraglal <szabotimi89@gmail.com>
2026-02-22 22:38:58 +0100

    Update Footer component image dimensions

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -16,7 +16,7 @@ export function Footer() {
               <ImageWithFallback
                   src="/pro-special.png"
                   alt="Pro Special logo"
-                  className="w-10svw h-10svw object-cover"
+                  className="w-auto h-auto object-cover"
                 />
               <span className="text-xl font-bold">Pro Special</span>
             </div>
