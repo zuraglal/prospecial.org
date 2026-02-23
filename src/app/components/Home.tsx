1cae0245215650b2a407772c239f66aa642aaf20
zuraglal <szabotimi89@gmail.com>
2026-02-23 15:20:33 +0100

    Update Home.tsx

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -39,7 +39,7 @@ export function Home() {
       <section id="donation" className="bg-gradient-to-br from-indigo-300 to-purple-300 py-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid lg:grid-cols-2 gap-12 items-center">
-            <div>
+            <div className="text-center mb-16">
               <h2 className="text-3xl lg:text-5xl font-bold text-gray-900 mb-4">
                 {getTranslation(language, 'donationTitle')}
               </h2>
